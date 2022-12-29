efae0ea92a249fec70c102fe7b47e8151363b847
lbbiplob <biplab.jsnu@gmail.com>
2022-12-29 23:14:43 +0600

    delete task added

--- src/Components/Pages/MyTask/MyTask/MyTask.js
+++ src/Components/Pages/MyTask/MyTask/MyTask.js
@@ -1,20 +1,56 @@
 import { useQuery } from "@tanstack/react-query";
+import { Table } from "flowbite-react";
 import React from "react";
 import Task from "../Task/Task";
 
 const MyTask = () => {
-  const { data: tasks = [] } = useQuery({
+  const { data: tasks = [], refetch } = useQuery({
     queryKey: ["task"],
     queryFn: () =>
       fetch("http://localhost:5000/task").then((res) => res.json()),
   });
+
+  const handelTaskDelete = (id) => {
+    const confirm = window.confirm(`Are you sure Delete This  task`);
+    if (confirm) {
+      fetch(`http://localhost:5000/task/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            refetch();
+          }
+        });
+    }
+  };
   return (
     <div>
       this is my task
-      <div>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task}></Task>
-        ))}
+      <div className="p-5">
+        <Table striped={true}>
+          <Table.Head>
+            <Table.HeadCell>Task</Table.HeadCell>
+            <Table.HeadCell>Name</Table.HeadCell>
+            <Table.HeadCell>Email</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+            <Table.HeadCell>Update</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {tasks.map((task) => (
+              <Task
+                key={task._id}
+                task={task}
+                handelTaskDelete={handelTaskDelete}
+              ></Task>
+            ))}
+          </Table.Body>
+        </Table>
+        ;
       </div>
     </div>
   );
--- src/Components/Pages/MyTask/Task/Task.js
+++ src/Components/Pages/MyTask/Task/Task.js
@@ -1,7 +1,36 @@
+import { Button, Table } from "flowbite-react";
 import React from "react";
 
-const Task = () => {
-  return <div>This is task</div>;
+const Task = ({ task, handelTaskDelete }) => {
+  const { name, userName, email, _id } = task;
+
+  return (
+    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+        {name}
+      </Table.Cell>
+      <Table.Cell>{userName}</Table.Cell>
+      <Table.Cell>{email}</Table.Cell>
+      <Table.Cell>
+        <Button size="xs" color="failure">
+          Complete
+        </Button>
+      </Table.Cell>
+      <Table.Cell>
+        <a
+          href="/tables"
+          className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+        >
+          Edit
+        </a>
+      </Table.Cell>
+      <Table.Cell>
+        <Button onClick={() => handelTaskDelete(_id)} size="xs" color="failure">
+          Delete
+        </Button>
+      </Table.Cell>
+    </Table.Row>
+  );
 };
 
 export default Task;
